5a6701cfcd7157da4e5b3e20a515c869dc6b5dd1
Sam Williams <sam@completecoding.io>
2023-01-06 18:14:52 +0100

    sentiment api

---
+++ src/functions/sentimentAnalysis/index.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { formatJSONResponse } from "@libs/api-gateway";
+
+import {
+  ComprehendClient,
+  DetectSentimentCommand,
+  DetectSentimentCommandInput,
+} from "@aws-sdk/client-comprehend";
+const comprehendClient = new ComprehendClient({});
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    const body = JSON.parse(event.body || "{}");
+
+    const { text } = body;
+    if (!text) {
+      throw Error('You need to pass up a field of "text" to analyse');
+    }
+
+    const res = await analyseSentiment({ text });
+
+    return formatJSONResponse(res);
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
+
+const analyseSentiment = async ({ text }: { text: string }) => {
+  const input: DetectSentimentCommandInput = {
+    Text: text,
+    LanguageCode: "en",
+  };
+
+  const command = new DetectSentimentCommand(input);
+  const response = await comprehendClient.send(command);
+  return response;
+};
